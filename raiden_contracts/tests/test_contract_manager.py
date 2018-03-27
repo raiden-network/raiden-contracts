2f5602394c9184cb92670f6b6b0e4d0c7735b7d8
Paul Lange <palango@gmx.de>
2018-03-27 16:18:37 +0200

    Add ContractManager tests for non-existant cases

--- raiden_contracts/tests/test_contract_manager.py
+++ raiden_contracts/tests/test_contract_manager.py
@@ -9,10 +9,16 @@ PRECOMPILED_CONTRACTS_PATH = 'raiden_contracts/data/contracts.json'
 
 def contract_manager_meta(contracts_path):
     manager = ContractManager(contracts_path)
+
     abi = manager.get_contract_abi('TokenNetwork')
     assert isinstance(abi, list)
+    with pytest.raises(KeyError):
+        manager.get_contract_abi('SomeName')
+
     abi = manager.get_event_abi('TokenNetwork', 'ChannelClosed')
     assert isinstance(abi, dict)
+    with pytest.raises(KeyError):
+        manager.get_event_abi('TokenNetwork', 'NonExistant')
 
 
 def test_contract_manager_compile():
