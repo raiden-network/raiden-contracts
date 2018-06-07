79497f65f0b02113238999a1f13fa1884e3d4b8b
Loredana <loredana.cirstea@gmail.com>
2018-06-07 03:11:25 +0200

    Fix channel settlement calculations
    We must first calculate the amount corresponding to the participant who closed the channel.
    Channel settlement was refactored and the above requirement was wrongly discarded.
    There have been previous discussions on the matter, the most important conclusion being https://github.com/raiden-network/raiden/issues/278#issuecomment-265177676

--- raiden_contracts/tests/fixtures/channel_test_values.py
+++ raiden_contracts/tests/fixtures/channel_test_values.py
@@ -8,15 +8,18 @@ ChannelValues = namedtuple('ChannelValues', [
     'locked'
 ])
 
+
 channel_settle_test_values = [
     (
         ChannelValues(deposit=40, withdrawn=10, transferred=26, locked=6),
-        ChannelValues(deposit=35, withdrawn=5, transferred=24, locked=4),
-        True  # settleChannel transaction should pass
+        ChannelValues(deposit=35, withdrawn=5, transferred=24, locked=4)
+    ),
+    (
+        ChannelValues(deposit=10, withdrawn=10, transferred=26, locked=0),
+        ChannelValues(deposit=5, withdrawn=5, transferred=24, locked=0)
     ),
     (
         ChannelValues(deposit=10, withdrawn=10, transferred=26, locked=6),
-        ChannelValues(deposit=5, withdrawn=5, transferred=24, locked=4),
-        False  # settleChannel transaction should fail
+        ChannelValues(deposit=5, withdrawn=5, transferred=24, locked=4)
     )
 ]
