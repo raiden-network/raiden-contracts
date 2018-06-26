eed31017099bfcae7f0f971d95de5773998858d2
err508 <theldt@posteo.de>
2018-06-26 19:02:15 +0200

    Resolves raiden/[...]/test_withdraw_twice. Adds test_unlock_twice_fails.

--- raiden_contracts/tests/test_channel_unlock.py
+++ raiden_contracts/tests/test_channel_unlock.py
@@ -163,6 +163,47 @@ def test_merkle_root(
     assert unlocked_amount == 9
 
 
+def test_unlock_twice_fails(
+        web3,
+        token_network,
+        get_accounts,
+        create_settled_channel,
+        reveal_secrets,
+):
+    (A, B) = get_accounts(2)
+    settle_timeout = 8
+
+    # Regular channel life-cycle: open -> settle -> unlock -> open -> settle -> unlock
+
+    # Mock pending transfers data
+    pending_transfers_tree_1 = get_pending_transfers_tree(web3, [1, 3, 5], [2, 4], settle_timeout)
+    reveal_secrets(A, pending_transfers_tree_1.unlockable)
+
+    # Settle the channel
+    create_settled_channel(
+        A,
+        pending_transfers_tree_1.locked_amount,
+        pending_transfers_tree_1.merkle_root,
+        B,
+        0,
+        EMPTY_MERKLE_ROOT,
+        settle_timeout,
+    )
+    token_network.functions.unlock(
+        A,
+        B,
+        pending_transfers_tree_1.packed_transfers,
+    ).transact({'from': A})
+
+    # Calling unlock twice does not work
+    with pytest.raises(TransactionFailed):
+        token_network.functions.unlock(
+            A,
+            B,
+            pending_transfers_tree_1.packed_transfers,
+        ).transact({'from': A})
+
+
 def test_unlock_wrong_locksroot(
         web3,
         token_network,
