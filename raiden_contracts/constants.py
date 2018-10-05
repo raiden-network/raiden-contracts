9d49071f266c801c713754c1be6dcaee27d56cdb
Lefteris Karapetsas <lefteris@refu.co>
2018-10-05 08:49:15 +0200

    Add chainid enum and config and better deployed address names

--- raiden_contracts/constants.py
+++ raiden_contracts/constants.py
@@ -16,18 +16,42 @@ CONTRACT_RAIDEN_SERVICE_BUNDLE = 'RaidenServiceBundle'
 # Deployed contract information
 # Deployed to Ropsten revival on 2018-09-03 from
 # raiden-contracts@fc1c79329a165c738fc55c3505cf801cc79872e4
-ROPSTEN_REGISTRY_ADDRESS = '0xf2a175A52Bd3c815eD7500c765bA19652AB89B30'
-ROPSTEN_DISCOVERY_ADDRESS = '0xEEADDC1667B6EBc7784721B123a6F669B69Eb9bD'
+ROPSTEN_TOKEN_NETWORK_REGISTRY_ADDRESS = '0xf2a175A52Bd3c815eD7500c765bA19652AB89B30'
+ROPSTEN_ENDPOINT_REGISTRY_ADDRESS = '0xEEADDC1667B6EBc7784721B123a6F669B69Eb9bD'
 ROPSTEN_SECRET_REGISTRY_ADDRESS = '0x16a25511A92C5ebfc6C30ad98F754e4c820c6822'
 # Deployed to Ropsten revival on 2018-09-21 from
 # raiden-contracts@bfb24fed3ebda2799e4d11ad1bb5a6de116bd12d
-ROPSTEN_LIMITS_REGISTRY_ADDRESS = '0x6cC27CBF184B4177CD3c5D1a39a875aD07345eEb'
-ROPSTEN_LIMITS_DISCOVERY_ADDRESS = '0xcF47EDF0D951c862ED9825F47075c15BEAf5Db1B'
+ROPSTEN_LIMITS_TOKEN_NETWORK_REGISTRY_ADDRESS = '0x6cC27CBF184B4177CD3c5D1a39a875aD07345eEb'
+ROPSTEN_LIMITS_ENDPOINT_REGISTRY_ADDRESS = '0xcF47EDF0D951c862ED9825F47075c15BEAf5Db1B'
 ROPSTEN_LIMITS_SECRET_REGISTRY_ADDRESS = '0x8167a262Fa3Be92F05420675c3b409c64Be3d348'
 
 
 # Network configurations
 START_QUERY_BLOCK_KEY = 'DefaultStartBlock'
+
+
+class ChainId(Enum):
+    MAINNET = 1
+    ROPSTEN = 3
+    RINKEBY = 4
+    KOVAN = 42
+    SMOKETEST = 627
+
+
+MAINNET = 'mainnet'
+ROPSTEN = 'ropsten'
+RINKEBY = 'rinkeby'
+KOVAN = 'kovan'
+SMOKETEST = 'smoketest'
+RAIDENTEST_CHAINID = 627
+
+ID_TO_NETWORKNAME = {
+    1: MAINNET,
+    3: ROPSTEN,
+    4: RINKEBY,
+    42: KOVAN,
+    RAIDENTEST_CHAINID: SMOKETEST,
+}
 
 
 class NetworkType(Enum):
@@ -40,9 +64,13 @@ ID_TO_NETWORK_CONFIG = {
         NetworkType.TEST: {
             'network_type': NetworkType.TEST,
             'contract_addresses': {
-                CONTRACT_ENDPOINT_REGISTRY: to_canonical_address(ROPSTEN_DISCOVERY_ADDRESS),
+                CONTRACT_ENDPOINT_REGISTRY: to_canonical_address(
+                    ROPSTEN_ENDPOINT_REGISTRY_ADDRESS,
+                ),
                 CONTRACT_SECRET_REGISTRY: to_canonical_address(ROPSTEN_SECRET_REGISTRY_ADDRESS),
-                CONTRACT_TOKEN_NETWORK_REGISTRY: to_canonical_address(ROPSTEN_REGISTRY_ADDRESS),
+                CONTRACT_TOKEN_NETWORK_REGISTRY: to_canonical_address(
+                    ROPSTEN_TOKEN_NETWORK_REGISTRY_ADDRESS,
+                ),
             },
             # 924 blocks before token network registry deployment
             START_QUERY_BLOCK_KEY: 3604000,
@@ -50,12 +78,14 @@ ID_TO_NETWORK_CONFIG = {
         NetworkType.MAIN: {
             'network_type': NetworkType.MAIN,
             'contract_addresses': {
-                CONTRACT_ENDPOINT_REGISTRY: to_canonical_address(ROPSTEN_LIMITS_DISCOVERY_ADDRESS),
+                CONTRACT_ENDPOINT_REGISTRY: to_canonical_address(
+                    ROPSTEN_LIMITS_ENDPOINT_REGISTRY_ADDRESS,
+                ),
                 CONTRACT_SECRET_REGISTRY: to_canonical_address(
                     ROPSTEN_LIMITS_SECRET_REGISTRY_ADDRESS,
                 ),
                 CONTRACT_TOKEN_NETWORK_REGISTRY: to_canonical_address(
-                    ROPSTEN_LIMITS_REGISTRY_ADDRESS,
+                    ROPSTEN_LIMITS_TOKEN_NETWORK_REGISTRY_ADDRESS,
                 ),
             },
             # 153 blocks before token network registry deployment
