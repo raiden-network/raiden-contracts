816809e8b394437541859eea4de03b0969ad1d8c
Loredana <loredana.cirstea@gmail.com>
2018-10-09 16:28:42 +0200

    Update tests to use constants.py version

--- raiden_contracts/constants.py
+++ raiden_contracts/constants.py
@@ -2,6 +2,7 @@ from enum import Enum, IntEnum
 
 from eth_utils import to_canonical_address
 
+# Do not change this, this is handled by bumpversion with .bumpversion_contracts.cfg
 CONTRACTS_VERSION = "0.3._"
 
 # Contract names
--- raiden_contracts/tests/test_secret_registry.py
+++ raiden_contracts/tests/test_secret_registry.py
@@ -1,9 +1,9 @@
 import pytest
 from web3 import Web3
 from web3.exceptions import ValidationError
-from raiden_contracts.constants import EVENT_SECRET_REVEALED
+from raiden_contracts.constants import EVENT_SECRET_REVEALED, CONTRACTS_VERSION
 from raiden_contracts.utils.events import check_secret_revealed, check_secrets_revealed
-from raiden_contracts.tests.fixtures.config import fake_bytes, CONTRACTS_VERSION
+from raiden_contracts.tests.fixtures.config import fake_bytes
 
 
 def test_version(secret_registry_contract):
