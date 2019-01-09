592c152e0d0fa15ef2d864bf66ef0680a205c029
Yoichi Hirai <yoichi@brainbot.com>
2019-01-09 12:10:00 +0100

    Add missing contractAddress attribute
    When the transaction receit misses a 'contractAddress' attribute,
    add it from the transaction info.
    
    This fixes #416.

--- raiden_contracts/utils/transaction.py
+++ raiden_contracts/utils/transaction.py
@@ -1,12 +1,14 @@
+from typing import Tuple
+
 from web3 import Web3
 from web3.utils.threads import (
     Timeout,
 )
 
 
-def check_succesful_tx(web3: Web3, txid: str, timeout=180) -> dict:
+def check_succesful_tx(web3: Web3, txid: str, timeout=180) -> Tuple[dict, dict]:
     '''See if transaction went through (Solidity code did not throw).
-    :return: Transaction receipt
+    :return: Transaction receipt and transaction info
     '''
     receipt = wait_for_transaction_receipt(web3, txid, timeout=timeout)
     txinfo = web3.eth.getTransaction(txid)
@@ -14,7 +16,7 @@ def check_succesful_tx(web3: Web3, txid: str, timeout=180) -> dict:
         raise ValueError(f"Status 0 indicates failure")
     if txinfo['gas'] == receipt['gasUsed']:
         raise ValueError(f"Gas is completely used ({txinfo['gas']}). Failure?")
-    return receipt
+    return (receipt, txinfo)
 
 
 def wait_for_transaction_receipt(web3, txid, timeout=180):
