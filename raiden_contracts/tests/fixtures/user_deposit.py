cd46cca383cd55ed941feef0b71c87a2e412a767
Karl Bartel <karl42@gmail.com>
2019-01-24 11:18:22 +0100

    Limit access to `transfer` to trusted contract

--- raiden_contracts/tests/fixtures/user_deposit.py
+++ raiden_contracts/tests/fixtures/user_deposit.py
@@ -7,9 +7,22 @@ from raiden_contracts.constants import CONTRACT_USER_DEPOSIT
 def user_deposit_contract(
     deploy_tester_contract,
     custom_token,
+    monitoring_service_external,
 ):
     return deploy_tester_contract(
         CONTRACT_USER_DEPOSIT,
         {},
         [custom_token.address],
+    )
+
+
+@pytest.fixture
+def udc_transfer_contract(
+    deploy_tester_contract,
+    user_deposit_contract,
+):
+    return deploy_tester_contract(
+        'UDCTransfer',
+        {},
+        [user_deposit_contract.address],
     )
