683f5a9b0b1af4f00c7f94eaaec788bc22ce15dd
Karl Bartel <karl42@gmail.com>
2019-02-04 18:09:33 +0100

    Add gas cost for most important service calls
    Closes #496.

--- raiden_contracts/tests/fixtures/monitoring_service.py
+++ raiden_contracts/tests/fixtures/monitoring_service.py
@@ -1,5 +1,6 @@
 import pytest
 from raiden_contracts.constants import CONTRACT_MONITORING_SERVICE
+from raiden_contracts.utils.proofs import sign_reward_proof
 
 
 @pytest.fixture()
@@ -25,3 +26,35 @@ def monitoring_service_external(
         service_registry.address,
         uninitialized_user_deposit_contract.address,
     ])
+
+
+@pytest.fixture()
+def create_reward_proof(token_network, get_private_key):
+    def get(
+            signer,
+            channel_identifier,
+            reward_amount,
+            token_network_address,
+            nonce=0,
+            v=27,
+    ):
+        private_key = get_private_key(signer)
+
+        signature = sign_reward_proof(
+            private_key,
+            channel_identifier,
+            reward_amount,
+            token_network_address,
+            int(token_network.functions.chain_id().call()),
+            nonce,
+            v,
+        )
+        return (
+            channel_identifier,
+            reward_amount,
+            token_network_address,
+            int(token_network.functions.chain_id().call()),
+            nonce,
+            signature,
+        )
+    return get
