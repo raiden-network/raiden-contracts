372b3e10ee4b32ea1b49c43267bd5ead4effcdf6
Yoichi Hirai <yoichi@brainbot.com>
2019-03-06 11:28:06 +0100

    Start testing etherscan_verify script

--- raiden_contracts/tests/test_etherscan_verify.py
+++ raiden_contracts/tests/test_etherscan_verify.py
@@ -1,5 +1,10 @@
+from typing import Dict
+
 from raiden_contracts.contract_manager import ContractManager, contracts_source_path
-from raiden_contracts.deploy.etherscan_verify import get_constructor_args
+from raiden_contracts.deploy.etherscan_verify import (
+    get_constructor_args,
+    post_data_for_etherscan_verification,
+)
 
 
 contract_name = 'DummyContract'
@@ -7,7 +12,7 @@ contract_name = 'DummyContract'
 
 def test_get_constructor_args_no_args():
     contract_manager = ContractManager(contracts_source_path())
-    deploy_info = {
+    deploy_info: Dict = {
         'contracts': {
             contract_name: {
                 'constructor_arguments': [],
@@ -58,3 +63,34 @@ def test_get_constructor_args_two_args():
     assert get_constructor_args(deploy_info, contract_name, contract_manager) == \
         '0000000000000000000000000000000000000000000000000000000000000010' \
         '0000000000000000000000000000000000000000000000000000000000000001'
+
+
+def test_post_data_for_etherscan_verification():
+    output = post_data_for_etherscan_verification(
+        apikey='jkl;jkl;jkl;',
+        deployment_info={'address': 'dummy_address'},
+        source='dummy_source',
+        contract_name=contract_name,
+        metadata={
+            'compiler': {
+                'version': '1.2.3',
+            },
+            'settings': {'optimizer': {
+                'enabled': False,
+                'runs': 'runs',
+            }},
+        },
+        constructor_args='constructor_arguments',
+    )
+    assert output == {
+        'apikey': 'jkl;jkl;jkl;',
+        'module': 'contract',
+        'action': 'verifysourcecode',
+        'contractaddress': 'dummy_address',
+        'sourceCode': 'dummy_source',
+        'contractname': contract_name,
+        'compilerversion': 'v1.2.3',
+        'optimizationUsed': 0,
+        'runs': 'runs',
+        'constructorArguements': 'constructor_arguments',
+    }
