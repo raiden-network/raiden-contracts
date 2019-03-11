eae38139f56d7f6b5a5a07a8cecdb704180c03aa
Yoichi Hirai <yoichi@brainbot.com>
2019-03-11 14:19:31 +0100

    Bump version: 0.9.0 → 0.10.0

--- raiden_contracts/constants.py
+++ raiden_contracts/constants.py
@@ -1,7 +1,7 @@
 from enum import Enum, IntEnum
 
 # Do not change this, this is handled by bumpversion with .bumpversion_contracts.cfg
-CONTRACTS_VERSION = '0.9.0'
+CONTRACTS_VERSION = '0.10.0'
 
 PRECOMPILED_DATA_FIELDS = ['abi', 'bin', 'bin-runtime', 'metadata']
 
