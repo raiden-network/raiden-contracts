56c3027abb6fc8a1606c7d2cc7b9fba0cd3c465a
Yoichi Hirai <yoichi@brainbot.com>
2019-03-27 16:48:56 +0100

    Branch out data_0.11.0

---
+++ raiden_contracts/data_0.11.0/source/raiden/Utils.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.5.4;
+
+/// @title Utils
+/// @notice Utils contract for various helpers used by the Raiden Network smart
+/// contracts.
+contract Utils {
+    string constant public contract_version = "0.11.0";
+
+    /// @notice Check if a contract exists
+    /// @param contract_address The address to check whether a contract is
+    /// deployed or not
+    /// @return True if a contract exists, false otherwise
+    function contractExists(address contract_address) public view returns (bool) {
+        uint size;
+
+        assembly {
+            size := extcodesize(contract_address)
+        }
+
+        return size > 0;
+    }
+}
