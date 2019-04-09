1496bbd6ce912c6e2ee48e313fb6b5112d7d2af3
Yoichi Hirai <yoichi@brainbot.com>
2019-04-09 19:00:20 +0200

    Test KeyError when tx receipt does not have status

--- raiden_contracts/tests/test_transaction.py
+++ raiden_contracts/tests/test_transaction.py
@@ -15,6 +15,17 @@ def test_check_successful_tx_with_status_zero():
     web3_mock.eth.getTransaction.assert_called_with(txid)
 
 
+def test_check_successful_tx_with_nonexistent_status():
+    """ check_successful_tx() with a receipt without status field should raise a KeyError """
+    web3_mock = Mock()
+    web3_mock.eth.getTransactionReceipt.return_value = {'blockNumber': 300}
+    txid = 'abcdef'
+    with pytest.raises(KeyError):
+        check_successful_tx(web3=web3_mock, txid=txid)
+    web3_mock.eth.getTransactionReceipt.assert_called_with(txid)
+    web3_mock.eth.getTransaction.assert_called_with(txid)
+
+
 def test_check_successful_tx_with_gas_completely_used():
     web3_mock = Mock()
     gas = 30000
