91f4cb135aaa6ee8dc803ab9063a630e818ff869
Yoichi Hirai <yoichi@brainbot.com>
2019-04-26 18:55:03 +0200

    Test to see call_and_transact() mines a block
    This is a part of #820.

--- raiden_contracts/tests/unit/test_web3_interaction.py
+++ raiden_contracts/tests/unit/test_web3_interaction.py
@@ -15,3 +15,12 @@ def test_logfilter_with_nonexistent_event(web3):
             from_block=0,
             to_block='latest',
         )
+
+
+def test_call_and_transact_does_not_mine(web3, custom_token):
+    """ See call_and_transact() does not mine a block """
+
+    before = web3.eth.blockNumber
+    custom_token.functions.multiplier().call_and_transact()
+    after = web3.eth.blockNumber
+    assert before + 1 == after
