4e4c0229f91abe6c389b6b840c701f1cadb9d57c
Yoichi Hirai <yoichi@brainbot.com>
2019-06-28 16:25:04 +0200

    Add a test case about mismatched contracts_version

--- raiden_contracts/tests/unit/test_merge_dict.py
+++ raiden_contracts/tests/unit/test_merge_dict.py
@@ -16,11 +16,25 @@ def test_merge_deployment_data_wrong_chain_id() -> None:
     """ merge_deployment_data() throws ValueError on two dictionaries with different chain_id's"""
     deployment1 = {
         "chain_id": 1,
-        "contract_version": "0.12.0",
+        "contracts_version": "0.12.0",
         "contracts": {"TokenNetworkRegistry": "something"},
     }
     deployment2 = deepcopy(deployment1)
     deployment2["chain_id"] = 2
+    deployment2["contracts"] = {"SecretRegistry": "something"}
+    with pytest.raises(ValueError):
+        merge_deployment_data(deployment1, deployment2)  # type: ignore
+
+
+def test_merge_deployment_data_wrong_contracts_version() -> None:
+    """ merge_deployment_data() throws ValueError on dicts with different contracts_version's"""
+    deployment1 = {
+        "chain_id": 1,
+        "contracts_version": "0.12.0",
+        "contracts": {"TokenNetworkRegistry": "something"},
+    }
+    deployment2 = deepcopy(deployment1)
+    deployment2["contracts_version"] = "0.13.0"
     deployment2["contracts"] = {"SecretRegistry": "something"}
     with pytest.raises(ValueError):
         merge_deployment_data(deployment1, deployment2)  # type: ignore
