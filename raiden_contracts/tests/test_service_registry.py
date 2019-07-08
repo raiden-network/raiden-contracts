6dba7472133627351fe3a86b655310a89abc3ba4
Yoichi Hirai <yoichi@brainbot.com>
2019-07-08 12:22:12 +0200

    Fixed tests

--- raiden_contracts/tests/test_service_registry.py
+++ raiden_contracts/tests/test_service_registry.py
@@ -16,35 +16,45 @@ def test_deposit(
     service_registry: Contract, custom_token: Contract, get_accounts: Callable
 ) -> None:
     (A,) = get_accounts(1)
-    custom_token.functions.mint(10).call_and_transact({"from": A})
-    custom_token.functions.approve(service_registry.address, 10).call_and_transact({"from": A})
+    custom_token.functions.mint(5000 * (10 ** 18)).call_and_transact({"from": A})
+    custom_token.functions.approve(
+        service_registry.address, 5000 * (10 ** 18)
+    ).call_and_transact({"from": A})
 
     # happy path
-    service_registry.functions.deposit(10).call_and_transact({"from": A})
-    assert service_registry.functions.deposits(A).call() == 10
-    assert custom_token.functions.balanceOf(A).call() == 0
+    old_balance = custom_token.functions.balanceOf(A).call()
+    current_price = service_registry.functions.current_price().call()
+    service_registry.functions.deposit(5000 * (10 ** 18)).call_and_transact({"from": A})
+    assert old_balance > custom_token.functions.balanceOf(A).call() > old_balance - current_price
 
     # custom_token does not allow transfer of more tokens
     with pytest.raises(TransactionFailed):
         service_registry.functions.deposit(1).call({"from": A})
 
 
-def test_setURL(service_registry: Contract, get_accounts: Callable) -> None:
+def test_setURL(custom_token: Contract, service_registry: Contract, get_accounts: Callable) -> None:
     (A,) = get_accounts(1)
     url1 = "http://example.com"
     url2 = "http://raiden.example.com"
 
-    # First setHost call, should add to `service_addresses`
+    custom_token.functions.mint(5000 * (10 ** 18)).call_and_transact({"from": A})
+    custom_token.functions.approve(
+        service_registry.address, 5000 * (10 ** 18)
+    ).call_and_transact({"from": A})
+    service_registry.functions.deposit(5000 * (10 ** 18)).call_and_transact({"from": A})
+
     service_registry.functions.setURL(url1).call_and_transact({"from": A})
     assert service_registry.functions.urls(A).call() == url1
-    assert service_registry.functions.service_addresses(0).call() == A
-    assert service_registry.functions.serviceCount().call() == 1
-    with pytest.raises(TransactionFailed):
-        assert service_registry.functions.service_addresses(1).call()
 
-    # Setting the host for the second time must not add the service address a
-    # second time.
     service_registry.functions.setURL(url2).call_and_transact({"from": A})
     assert service_registry.functions.urls(A).call() == url2
-    assert service_registry.functions.service_addresses(0).call() == A
-    assert service_registry.functions.serviceCount().call() == 1
+
+
+def test_decayed_price(service_registry: Contract) -> None:
+    assert service_registry.functions.decayed_price(100000, 0).call() == 100000
+
+    # The minimum price is 1000
+    assert service_registry.functions.decayed_price(100, 0).call() == 1000
+
+    # roughly 139 days till the price halves.
+    assert service_registry.functions.decayed_price(100000, 11990300).call() == 50000
