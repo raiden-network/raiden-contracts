982eac8d4dc421a561a74800243e0b4a89797269
Karl Bartel <karl42@gmail.com>
2019-07-30 16:27:52 +0200

    Extract `make_iou` out of `test_claim`

--- raiden_contracts/tests/test_one_to_n.py
+++ raiden_contracts/tests/test_one_to_n.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 import pytest
 from eth_tester.exceptions import TransactionFailed
+from eth_typing.evm import HexAddress
 from web3 import Web3
 from web3.contract import Contract
 
@@ -9,114 +10,94 @@ from raiden_contracts.constants import OneToNEvent
 from raiden_contracts.utils.proofs import sign_one_to_n_iou
 
 
+@pytest.fixture
+def make_iou(web3: Web3, one_to_n_contract: Contract, get_private_key: Callable) -> Callable:
+    chain_id = int(web3.version.network)
+
+    def f(
+        sender: HexAddress,
+        receiver: HexAddress,
+        amount: int = 10,
+        expiration_block: int = None,
+        chain_id: int = chain_id,
+        one_to_n_address: HexAddress = one_to_n_contract.address,
+    ) -> dict:
+        if expiration_block is None:
+            expiration_block = web3.eth.blockNumber + 10
+        iou = dict(
+            sender=sender,
+            receiver=receiver,
+            amount=amount,
+            expiration_block=expiration_block,
+            one_to_n_address=one_to_n_address,
+            chain_id=chain_id,
+        )
+        iou["signature"] = sign_one_to_n_iou(get_private_key(sender), **iou)
+        del iou["chain_id"]
+        return iou
+
+    return f
+
+
 def test_claim(
     user_deposit_contract: Contract,
     one_to_n_contract: Contract,
     deposit_to_udc: Callable,
     get_accounts: Callable,
-    get_private_key: Callable,
     web3: Web3,
     event_handler: Callable,
+    make_iou: Callable,
 ) -> None:
     ev_handler = event_handler(one_to_n_contract)
     (A, B) = get_accounts(2)
     deposit_to_udc(A, 30)
 
-    # happy case
-    amount = 10
-    expiration = web3.eth.blockNumber + 2
-    chain_id = int(web3.version.network)
-
     # IOU expired
     with pytest.raises(TransactionFailed):
         bad_expiration = web3.eth.blockNumber - 1
-        signature = sign_one_to_n_iou(
-            get_private_key(A),
-            sender=A,
-            receiver=B,
-            amount=amount,
-            expiration_block=bad_expiration,
-            one_to_n_address=one_to_n_contract.address,
-            chain_id=chain_id,
-        )
         one_to_n_contract.functions.claim(
-            A, B, amount, bad_expiration, one_to_n_contract.address, signature
+            **make_iou(sender=A, receiver=B, expiration_block=bad_expiration)
         ).call({"from": A})
 
     # Wrong OneToN address
     with pytest.raises(TransactionFailed):
-        signature = sign_one_to_n_iou(
-            get_private_key(A),
-            sender=A,
-            receiver=B,
-            amount=amount,
-            expiration_block=expiration,
-            one_to_n_address=A,  # Inject an error
-            chain_id=chain_id,
-        )
-        one_to_n_contract.functions.claim(A, B, amount, expiration, A, signature).call({"from": A})
+        one_to_n_contract.functions.claim(
+            **make_iou(sender=A, receiver=B, one_to_n_address=A)
+        ).call({"from": A})
 
     # Wrong chain_id
     with pytest.raises(TransactionFailed):
-        signature = sign_one_to_n_iou(
-            get_private_key(A),
-            sender=A,
-            receiver=B,
-            amount=amount,
-            expiration_block=expiration,
-            one_to_n_address=one_to_n_contract.address,
-            chain_id=chain_id + 2,  # Inject an error
+        one_to_n_contract.functions.claim(**make_iou(sender=A, receiver=B, chain_id=77)).call(
+            {"from": A}
         )
-        one_to_n_contract.functions.claim(A, B, amount, expiration, A, signature).call({"from": A})
 
     # bad signature
     with pytest.raises(TransactionFailed):
-        expiration = web3.eth.blockNumber + 1
-        signature = sign_one_to_n_iou(
-            get_private_key(A),
-            sender=A,
-            receiver=B,
-            amount=amount + 1,  # this does not match amount below
-            expiration_block=expiration,
-            one_to_n_address=one_to_n_contract.address,
-            chain_id=chain_id,
-        )
-        one_to_n_contract.functions.claim(
-            A, B, amount, expiration, one_to_n_contract.address, signature
-        ).call({"from": A})
+        iou = make_iou(sender=A, receiver=B, amount=10)
+        iou2 = make_iou(sender=A, receiver=B, amount=11)
+        iou["signature"] = iou2["signature"]  # use signature for wrong amount
+        one_to_n_contract.functions.claim(**iou).call({"from": A})
 
-    signature = sign_one_to_n_iou(
-        get_private_key(A),
-        sender=A,
-        receiver=B,
-        amount=amount,
-        expiration_block=expiration,
-        one_to_n_address=one_to_n_contract.address,
-        chain_id=chain_id,
-    )
-
-    tx_hash = one_to_n_contract.functions.claim(
-        sender=A,
-        receiver=B,
-        amount=amount,
-        expiration_block=expiration,
-        one_to_n_address=one_to_n_contract.address,
-        signature=signature,
-    ).call_and_transact({"from": A})
+    # happy case
+    iou = make_iou(sender=A, receiver=B)
+    tx_hash = one_to_n_contract.functions.claim(**iou).call_and_transact({"from": A})
 
     ev_handler.assert_event(
         tx_hash,
         OneToNEvent.CLAIMED,
-        dict(sender=A, receiver=B, expiration_block=expiration, transferred=amount),
+        dict(
+            sender=A,
+            receiver=B,
+            expiration_block=iou["expiration_block"],
+            transferred=iou["amount"],
+        ),
     )
     assert user_deposit_contract.functions.balances(A).call() == 20
     assert user_deposit_contract.functions.balances(B).call() == 10
 
     # can't be claimed twice
     with pytest.raises(TransactionFailed):
-        one_to_n_contract.functions.claim(
-            A, B, amount, expiration, one_to_n_contract.address, signature
-        ).call({"from": A})
+        one_to_n_contract.functions.claim(**iou).call({"from": A})
 
 
 def test_claim_with_insufficient_deposit(
