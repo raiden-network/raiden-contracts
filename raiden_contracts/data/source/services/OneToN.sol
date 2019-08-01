eeabaf872c78b5a3461e8bc518894fa194d8189f
Karl Bartel <karl42@gmail.com>
2019-08-01 14:25:09 +0200

    Add bulkClaim to OneToN to allow saving gas
    This eliminates the overhead of requiring one transaction per claimed
    IOU.
    
    Closes https://github.com/raiden-network/raiden-contracts/issues/542.

--- raiden_contracts/data/source/services/OneToN.sol
+++ raiden_contracts/data/source/services/OneToN.sol
@@ -55,6 +55,7 @@ contract OneToN is Utils {
     /// @param receiver Address to which the amount is transferred
     /// @param amount Owed amount of tokens
     /// @param expiration_block Tokens can only be claimed before this time
+    /// @param one_to_n_address Address of this contract
     /// @param signature Sender's signature over keccak256(sender, receiver, amount, expiration_block)
     /// @return Amount of transferred tokens
     function claim(
@@ -95,13 +96,84 @@ contract OneToN is Utils {
             assert(expiration_block > 0);
             emit Claimed(sender, receiver, expiration_block, transferable);
 
-            // event SessionSettled(_key, expiration_block);
             require(deposit_contract.transfer(sender, receiver, transferable), "deposit did not transfer");
         }
         return transferable;
     }
 
-    // TODO: gas saving function to claim multiple IOUs and free space in one transaction
+    /// @notice Submit multiple IOUs to claim the owed amount.
+    /// This is the same as calling `claim` multiple times, except for the reduced gas cost.
+    /// @param senders Addresses from which the amounts are transferred
+    /// @param receivers Addresses to which the amounts are transferred
+    /// @param amounts Owed amounts of tokens
+    /// @param expiration_blocks Tokens can only be claimed before this time
+    /// @param one_to_n_address Address of this contract
+    /// @param signatures Sender's signatures concatenated into a single bytes array
+    /// @return Amount of transferred tokens
+    function bulkClaim(
+        address[] memory senders,
+        address[] memory receivers,
+        uint256[] memory amounts,
+        uint256[] memory expiration_blocks,
+        address one_to_n_address,
+        bytes memory signatures
+    )
+        public
+        returns (uint)
+    {
+        uint256 transferable = 0;
+        require(
+            senders.length == receivers.length &&
+            senders.length == amounts.length &&
+            senders.length == expiration_blocks.length,
+            "Same number of elements required for all input parameters"
+        );
+        require(
+            signatures.length == senders.length * 65,
+            "`signatures` should contain 65 bytes per IOU"
+        );
+        for (uint256 i = 0; i < senders.length; i++) {
+            transferable += claim(
+                senders[i],
+                receivers[i],
+                amounts[i],
+                expiration_blocks[i],
+                one_to_n_address,
+                getSingleSignature(signatures, i)
+            );
+        }
+        return transferable;
+    }
+
+    /// @notice Get a single signature out of a byte array that contains concatenated signatures.
+    /// @param signatures Multiple signatures concatenated into a single byte array
+    /// @param i Index of the requested signature (zero based)
+    function getSingleSignature(
+        bytes memory signatures,
+        uint256 i
+    )
+        // TODO: make internal
+        public
+        pure
+        returns (bytes memory)
+    {
+        uint256 offset = i * 65;
+        // We need only 65, but we can access only whole words, so the next usable size is 3 * 32.
+        bytes memory signature = new bytes(96);
+        assembly { // solium-disable-line security/no-inline-assembly
+            // Copy the 96 bytes, using `offset` to start at the beginning
+            // of the requested signature.
+            mstore(add(signature, 32), mload(add(add(signatures, 32), offset)))
+            mstore(add(signature, 64), mload(add(add(signatures, 64), offset)))
+            mstore(add(signature, 96), mload(add(add(signatures, 96), offset)))
+
+            // The first 32 bytes store the length of the dynamic array.
+            // Since a signature is 65 bytes, we set the length to 65, so
+            // that only the signature is returned.
+            mstore(signature, 65)
+        }
+        return signature;
+    }
 
     /*
      *  Internal Functions
