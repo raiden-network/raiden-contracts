0cdae18fa7266c05d492b11e21e9b7304533dc1d
Yoichi Hirai <i@yoichihirai.com>
2019-09-02 18:12:42 +0200

    Update raiden_contracts/tests/test_service_registry.py
    Co-Authored-By: Karl Bartel <karl42@gmail.com>

--- raiden_contracts/tests/test_service_registry.py
+++ raiden_contracts/tests/test_service_registry.py
@@ -239,7 +239,7 @@ def test_changing_min_price(service_registry: Contract) -> None:
 
 
 def test_changing_min_price_above_current(service_registry: Contract) -> None:
-    """Changin min_price above the current price."""
+    """Changing min_price above the current price."""
     current_price = service_registry.functions.currentPrice().call()
     service_registry.functions.changeParameters(
         _price_bump_numerator=DEFAULT_BUMP_NUMERATOR,
