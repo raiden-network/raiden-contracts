0642dd0b4ebffc7d4cc6d2e212ff6479184f210e
Yoichi Hirai <yoichi@brainbot.com>
2019-10-14 15:49:19 +0200

    Better error message for missing command argument
    When --token-network-registry-address was missing, the
    deployment script showed a mysterious error
    ```
    TypeError: One or more arguments could not be encoded to the necessary ABI type.  Expected types are: address, address, address, address
    ```
    
    After this commit, the error message is much easier.
    ```
    Error: Missing option "--token-network-registry-address".
    ```
    
    This fixes #1168.

--- raiden_contracts/utils/versions.py
+++ raiden_contracts/utils/versions.py
@@ -44,3 +44,20 @@ def contracts_version_has_initial_service_deposit(version: Optional[str]) -> boo
     if version == "0.8.0_unlimited":
         return False
     return compare(version, "0.18.0") > 0
+
+
+def contracts_version_monitoring_service_takes_token_network_registry(
+    version: Optional[str]
+) -> bool:
+    """ Returns true if the contracts_version's MonitoringService contracts
+
+    expects a TokenNetworkRegistry address as a constructor argument.
+    """
+    if version is None:
+        # stock version in `data`
+        return True
+    if version == "0.3._":
+        return False
+    if version == "0.8.0_unlimited":
+        return False
+    return compare(version, "0.22.0") > 0
