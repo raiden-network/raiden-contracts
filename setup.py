d7b6dd53b8cd3bb015d331717e94ed0b84bbef44
Karl Bartel <karl42@gmail.com>
2020-01-23 11:08:30 +0100

    Bump version: 0.34.0 → 0.35.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools.command.build_py import build_py
 
 
 DESCRIPTION = 'Raiden contracts library and utilities'
-VERSION = '0.34.0'
+VERSION = '0.35.0'
 
 
 def read_requirements(path: str) -> List[str]:
