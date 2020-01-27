9b900203c517db210b81893d41ac60ed20f7e205
Paul Lange <palango@gmx.de>
2020-01-27 15:04:31 +0100

    Update solc to 0.5.16

--- setup.py
+++ setup.py
@@ -10,9 +10,8 @@ from typing import List
 from setuptools import Command
 from setuptools.command.build_py import build_py
 
-
-DESCRIPTION = 'Raiden contracts library and utilities'
-VERSION = '0.35.0'
+DESCRIPTION = "Raiden contracts library and utilities"
+VERSION = "0.35.0"
 
 
 def read_requirements(path: str) -> List[str]:
@@ -28,14 +27,14 @@ def _get_single_requirement(requirements: List[str], package: str) -> List[str]:
 class BuildPyCommand(build_py):
     def run(self) -> None:
         try:
-            self.run_command('verify_contracts')
+            self.run_command("verify_contracts")
         except SystemExit:
             pass
         build_py.run(self)
 
 
 class VerifyContracts(Command):
-    description = 'Verify that the compiled contracts have the correct source code checksum'
+    description = "Verify that the compiled contracts have the correct source code checksum"
     user_options: List = []
 
     def initialize_options(self) -> None:
@@ -45,19 +44,18 @@ class VerifyContracts(Command):
         pass
 
     def run(self) -> None:  # pylint: disable=no-self-use
-        from raiden_contracts.contract_manager import (
-            contracts_precompiled_path,
-        )
+        from raiden_contracts.contract_manager import contracts_precompiled_path
         from raiden_contracts.contract_source_manager import (
             ContractSourceManager,
             contracts_source_path,
         )
+
         manager = ContractSourceManager(contracts_source_path(contracts_version=None))
         manager.verify_precompiled_checksums(contracts_precompiled_path())
 
 
 class CompileContracts(Command):
-    description = 'Compile contracts and add ABI and checksums to a json file'
+    description = "Compile contracts and add ABI and checksums to a json file"
     user_options: List = []
 
     def initialize_options(self) -> None:
@@ -77,41 +75,39 @@ class CompileContracts(Command):
         contract_manager.compile_contracts(contracts_precompiled_path())
 
 
-requirements = read_requirements('requirements.txt')
+requirements = read_requirements("requirements.txt")
 
 config = {
-    'version': VERSION,
-    'scripts': [],
-    'name': 'raiden-contracts',
-    'author': 'Brainbot Labs Est.',
-    'author_email': 'contact@brainbot.li',
-    'description': DESCRIPTION,
-    'url': 'https://github.com/raiden-network/raiden-contracts/',
-    'license': 'MIT',
-    'keywords': 'raiden ethereum blockchain',
-    'install_requires': requirements,
-    'setup_requires': _get_single_requirement(requirements, 'py-solc'),
-    'packages': find_packages(),
-    'include_package_data': True,
-    'classifiers': [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+    "version": VERSION,
+    "scripts": [],
+    "name": "raiden-contracts",
+    "author": "Brainbot Labs Est.",
+    "author_email": "contact@brainbot.li",
+    "description": DESCRIPTION,
+    "url": "https://github.com/raiden-network/raiden-contracts/",
+    "license": "MIT",
+    "keywords": "raiden ethereum blockchain",
+    "install_requires": requirements,
+    "setup_requires": _get_single_requirement(requirements, "py-solc"),
+    "packages": find_packages(),
+    "include_package_data": True,
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    'entry_points': {
-        'console_scripts': ['deploy = raiden_contracts.deploy.__main__:main'],
+    "entry_points": {"console_scripts": ["deploy = raiden_contracts.deploy.__main__:main"],},
+    "cmdclass": {
+        "compile_contracts": CompileContracts,
+        "verify_contracts": VerifyContracts,
+        "build_py": BuildPyCommand,
     },
-    'cmdclass': {
-        'compile_contracts': CompileContracts,
-        'verify_contracts': VerifyContracts,
-        'build_py': BuildPyCommand,
-    },
-    'zip_safe': False,
-    'package_data': {"raiden_contracts": ["py.typed"]},
+    "zip_safe": False,
+    "package_data": {"raiden_contracts": ["py.typed"]},
 }
 
 setup(**config)
