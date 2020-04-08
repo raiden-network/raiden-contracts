6ccaccf003a666a9efea51bc5a07465e8c78070d
Konrad Feldmeier <konrad@brainbot.com>
2020-04-08 13:24:05 +0200

    Bump version: 0.37.0-beta → 0.37.0b2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import Command
 from setuptools.command.build_py import build_py
 
 DESCRIPTION = "Raiden contracts library and utilities"
-VERSION = "0.37.0-beta"
+VERSION = "0.37.0b2"
 
 
 def read_requirements(path: str) -> List[str]:
