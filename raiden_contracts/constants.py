b52ec4c120368184bdceb71a5173b3a29f4c7098
Karl Bartel <karl42@gmail.com>
2020-04-09 11:09:41 +0200

    Update CONTRACTS_VERSION and CHANGELOG

--- raiden_contracts/constants.py
+++ raiden_contracts/constants.py
@@ -8,7 +8,7 @@ from eth_utils import keccak
 from raiden_contracts.utils.type_aliases import ChainID
 
 # The last digit is supposed to be zero always. See `RELEASE.rst`.
-CONTRACTS_VERSION = "0.37.0-beta"
+CONTRACTS_VERSION = "0.37.0-b2"
 
 PRECOMPILED_DATA_FIELDS = ["abi", "bin", "bin-runtime", "metadata"]
 
