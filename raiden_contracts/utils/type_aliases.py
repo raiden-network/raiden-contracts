e04642f9599de76e643159a61fdfa30713560ec1
Karl Bartel <karl42@gmail.com>
2020-06-04 15:38:56 +0200

    Include basic types from Raiden
    These can be imported from Raiden, so that both packages share the same
    types for basic Raiden objects.
    
    Closes https://github.com/raiden-network/raiden-contracts/issues/1291.

--- raiden_contracts/utils/type_aliases.py
+++ raiden_contracts/utils/type_aliases.py
@@ -1,4 +1,26 @@
 from typing import NewType
 
+T_AdditionalHash = bytes
+AdditionalHash = NewType("AdditionalHash", T_AdditionalHash)
+
+T_BalanceHash = bytes
+BalanceHash = NewType("BalanceHash", T_BalanceHash)
+
+# An absolute number of blocks
+T_BlockExpiration = int
+BlockExpiration = NewType("BlockExpiration", T_BlockExpiration)
+
 T_ChainID = int
 ChainID = NewType("ChainID", T_ChainID)
+
+T_ChannelID = int
+ChannelID = NewType("ChannelID", T_ChannelID)
+
+T_PrivateKey = bytes
+PrivateKey = NewType("PrivateKey", T_PrivateKey)
+
+T_Signature = bytes
+Signature = NewType("Signature", T_Signature)
+
+T_TokenAmount = int
+TokenAmount = NewType("TokenAmount", T_TokenAmount)
