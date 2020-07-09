6b52c7f3dbef770853d5a77118d5875710a3a47d
Karl Bartel <karl42@gmail.com>
2020-07-09 14:27:49 +0200

    Rewrite more asserts for better test failures

--- raiden_contracts/tests/conftest.py
+++ raiden_contracts/tests/conftest.py
@@ -1,2 +1,9 @@
-# pylint: disable=W0614, C0111, W0401
+"""
+isort:skip_file
+"""
+from pytest import register_assert_rewrite
+
+register_assert_rewrite("raiden_contracts.utils.events")
+register_assert_rewrite("raiden_contracts.tests.fixtures.channel")
+
 from .fixtures import *  # noqa: F401,F403
