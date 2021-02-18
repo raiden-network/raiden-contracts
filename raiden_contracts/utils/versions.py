a0bd79ec6df30d005bc7d54266a728ca6a53c80e
Paul Lange <palango@gmx.de>
2021-02-18 12:06:05 +0100

    Replace deprecated `semver.compare`

--- raiden_contracts/utils/versions.py
+++ raiden_contracts/utils/versions.py
@@ -1,27 +1,27 @@
 from typing import Optional
 
-from semver import compare
+from semver import VersionInfo
 
 
 def contracts_version_with_max_token_networks(version: Optional[str]) -> bool:
     if version is None:
         # contracts_version == None means the stock version in development.
         return True
-    return compare(version, "0.9.0") >= 0
+    return VersionInfo.parse(version).compare("0.9.0") >= 0
 
 
 def contracts_version_provides_services(version: Optional[str]) -> bool:
     if version is None:
         # contracts_version == None means the stock version in development.
         return True
-    return compare(version, "0.8.0") >= 0
+    return VersionInfo.parse(version).compare("0.8.0") >= 0
 
 
 def contracts_version_has_initial_service_deposit(version: Optional[str]) -> bool:
     if version is None:
         # contracts_versoin == None means the stock version in development.
         return True
-    return compare(version, "0.18.0") > 0
+    return VersionInfo.parse(version).compare("0.18.0") > 0
 
 
 def contracts_version_monitoring_service_takes_token_network_registry(
@@ -34,4 +34,4 @@ def contracts_version_monitoring_service_takes_token_network_registry(
     if version is None:
         # stock version in `data`
         return True
-    return compare(version, "0.22.0") > 0
+    return VersionInfo.parse(version).compare("0.22.0") > 0
