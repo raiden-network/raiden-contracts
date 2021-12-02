a00ce6124ecdebbe033eced37515217826fb9f99
Paul Lange <palango@gmx.de>
2021-12-02 14:51:33 +0100

    chore: Update 0.40.0 deployment directory

--- raiden_contracts/data_0.40.0/source/test/CustomToken.sol
+++ raiden_contracts/data_0.40.0/source/test/CustomToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 /* solium-disable error-reason */
-pragma solidity 0.8.7;
+pragma solidity 0.8.10;
 
 /*
 This Token Contract implements the standard token functionality (https://github.com/ethereum/EIPs/issues/20), the ERC223 functionality (https://github.com/ethereum/EIPs/issues/223) as well as the following OPTIONAL extras intended for use by humans.
