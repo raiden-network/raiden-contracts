6725714739f4eabe1d09f6108b3ba87904f9fd8c
Karl Bartel <karl42@gmail.com>
2022-10-13 15:20:29 +0200

    Drop support for python3.7

--- setup.py
+++ setup.py
@@ -97,7 +97,6 @@ config = {
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
